3f4a744d31a182df27840342070ac32c99708a11
KillerMech <Sephy7Killermech@gmail.com>
2024-02-13 17:02:17 -0500

    server update before thread update

--- project_c2_rust/src/server.rs
+++ project_c2_rust/src/server.rs
@@ -25,20 +25,21 @@ pub fn start_server() {
 fn handle_client(mut stream: TcpStream) {
     // TODO: Implement this function
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, filename) = if request_line.starts_with("GET / HTTP/1.1") {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = 
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    stream.write(response.as_bytes()).unwrap();
     stream.shutdown(std::net::Shutdown::Both).unwrap();
 }
 
