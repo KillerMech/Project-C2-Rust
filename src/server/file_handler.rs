f2a81f1bb486a18550ae285125b12a28b5d2cf25
Matthew <sephy7killermech@gmail.com>
2024-03-21 19:56:48 -0400

    created broken file module

---
+++ src/server/file_handler.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::Write;
+
+pub fn manage_listener_file(request: &String, identifier: &String) -> Result<String, String> {
+    if request == "new_listener" {
+        let mut file = File::options()
+            .append(true)
+            .open("./listener.txt").unwrap();
+
+        if (file.metadata().unwrap().len() as i64) == 0 {
+            file.write(format!("{}", identifier).as_bytes()).unwrap();
+            Result::Ok("Success".to_string())
+        } else if (file.metadata().unwrap().len()) != 0 {
+            file.write(format!(",{}",identifier).as_bytes()).expect("write failed");
+            Result::Ok("Success".to_string())
+        } else {
+            Result::Err("Couldn't save new listener to file".to_string())
+        }
+    } else if request == "delete_listener" {
+        let mut file = File::options()
+            .write(true)
+            .open("./listener.txt").expect("cannot open file");
+
+
+
+        file.write_all(identifier.as_bytes()).unwrap();
+        Result::Ok("Success".to_string())
+    } else {
+        Result::Err("This didn't work".to_string())
+    }
+
+}
